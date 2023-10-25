e5e3cfb73e961a59ca2a17f8c1eeba0efa830758
Rule Lulu <105763198+RuleLuluDamara@users.noreply.github.com>
2023-10-25 13:40:55 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index" :style="getBackgroundStyle(index)">
+        {{ todo.teks }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [{ teks: "Learn Javascript - (Belajar JavaScript dari nol)" }, { teks: "Learn Vue - (Belajar Vue dari nol)" }, { teks: "Build Something awesome - (Belajar JavaScript dari Vue dari Nol)" }],
+    };
+  },
+  methods: {
+    getBackgroundStyle(index) {
+      if (index === 0 || index === 2) {
+        return {
+          backgroundColor: "red",
+          /* You can adjust other styles as needed */
+        };
+      }
+      return {}; // Return an empty object if no special style is needed
+    },
+  },
+};
+</script>
